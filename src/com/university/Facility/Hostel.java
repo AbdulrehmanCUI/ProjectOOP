5a4ea92e61b7e78607ee852a6cbf658ee3ee543a
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-14 12:04:59 -0700

    hostel class done

--- src/com/university/Facility/Hostel.java
+++ src/com/university/Facility/Hostel.java
@@ -1,4 +1,43 @@
 package com.university.Facility;
 
 public class Hostel {
+    private int rooms;
+    private String warden;
+    private int students;
+
+    Hostel() {
+        rooms = 0;
+        warden = "Unknown";
+        students = 0;
+    }
+
+    Hostel(int rooms, String warden, int students) {
+        this.rooms = rooms;
+        this.warden = warden;
+        this.students = students;
+    }
+
+    public void setRooms(int rooms) {
+        this.rooms = rooms;
+    }
+
+    public int getRooms() {
+        return rooms;
+    }
+
+    public void setWarden(String warden) {
+        this.warden = warden;
+    }
+
+    public String getWarden() {
+        return warden;
+    }
+
+    public void setStudents(int students) {
+        this.students = students;
+    }
+
+    public int getStudents() {
+        return students;
+    }
 }
