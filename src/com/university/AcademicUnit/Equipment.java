1f7c92c16d319b9c4bdfa3f00d61f9bc3927c318
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-13 20:36:40 -0700

    equipment class done

--- src/com/university/AcademicUnit/Equipment.java
+++ src/com/university/AcademicUnit/Equipment.java
@@ -1,5 +1,32 @@
 package com.university.AcademicUnit;
 
 public class Equipment {
+    private int equipmentId;
+    private String equipmentName;
+    private String status;
+
+    public int getEquipmentId() {
+        return equipmentId;
+    }
+
+    public void setEquipmentId(int equipmentId) {
+        this.equipmentId = equipmentId;
+    }
+
+    public String getEquipmentName() {
+        return equipmentName;
+    }
+
+    public void setEquipmentName(String equipmentName) {
+        this.equipmentName = equipmentName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
 
