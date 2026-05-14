42a9a957be10029cd10b52eb0cc8753842075c58
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-14 12:00:00 -0700

    class complete

--- src/com/university/Facility/Book.java
+++ src/com/university/Facility/Book.java
@@ -1,4 +1,39 @@
 package com.university.Facility;
 
 public class Book {
-}
+
+    private String title;
+    private String author;
+    private int pages;
+
+    Book() {
+        title = "No Title";
+        author = "Unknown";
+        pages = 0;
+    }
+
+    Book(String title, String author, int pages) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setPages(int pages) {
+        this.pages = pages;
+    }
