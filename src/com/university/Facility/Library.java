033a34fab53ac0d3f8934eaa601a87981cf2d651
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-14 12:01:01 -0700

    libraray class done

--- src/com/university/Facility/Library.java
+++ src/com/university/Facility/Library.java
@@ -1,4 +1,43 @@
 package com.university.Facility;
 
 public class Library {
+    private String librarian;
+    private int totalBooks;
+    private Book book;
+
+    Library() {
+        librarian = "Unknown";
+        totalBooks = 0;
+        book = new Book();
+    }
+
+    Library(String librarian, int totalBooks, Book book) {
+        this.librarian = librarian;
+        this.totalBooks = totalBooks;
+        this.book = book;
+    }
+
+    public void setLibrarian(String librarian) {
+        this.librarian = librarian;
+    }
+
+    public String getLibrarian() {
+        return librarian;
+    }
+
+    public void setTotalBooks(int totalBooks) {
+        this.totalBooks = totalBooks;
+    }
+
+    public int getTotalBooks() {
+        return totalBooks;
+    }
+
+    public void setBook(Book book) {
+        this.book = book;
+    }
+
+    public Book getBook() {
+        return book;
+    }
 }
