a052a2510b20841f2d0daf4ee3e800a41a691cff
Abdulrehman <abdulrehman003cui@gmail.com>
2026-05-16 01:30:35 +0500

    ye CampusRepository hochuki hui complete ab kl mene Service Unit pr kaam krna itne mein tum ye krna na k poore question ki UML diagram bna lena... wo uska tareeka pooch lena mere se

--- src/com/university/CampusRepository.java
+++ src/com/university/CampusRepository.java
@@ -8,36 +8,48 @@ import java.util.ArrayList;
 
 public class CampusRepository<T extends Identifiable> implements Repository<T> {
     ArrayList<T> list = new ArrayList<>();
+
+    // Add method
     @Override
     public void add(T obj) {
         list.add(obj);
     }
 
+    // Delete
     @Override
     public void delete(int id) {
         for(int i = 0; i < list.size();i++){
             if(list.get(i).getID() == id){
                 list.remove(i);
+                return;
             }
         }
         System.out.println("Successfully Removed...");
     }
 
+    // Update method
     @Override
     public void update(int index, T obj) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getID() == obj.getID()) {
-                list.set( ,obj);
+                list.set(i,obj);
                 return;
             }
         }
     }
 
+    // getID method
     @Override
     public T getID(int id) {
+        for(T obj : list){
+            if(obj.getID() == id){
+                return obj;
+            }
+        }
         return null;
     }
 
+    // All the items in the array list
     public ArrayList<T> getAll(){
         return list;
     }
--- src/com/university/CampusZone.java
+++ src/com/university/CampusZone.java
@@ -1,5 +1,5 @@
 package com.university;
-import
+
 import com.university.Facility.*;
 import com.university.ServiceUnit.*;
 
