474aa008b109ff2ab511ff6785b8da9ceaadc9fd
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-15 12:55:43 -0700

    class done

--- src/com/university/CampusZone.java
+++ src/com/university/CampusZone.java
@@ -1,4 +1,26 @@
 package com.university;
 
 public class CampusZone {
+        Library library;
+        Cafeteria cafeteria;
+
+        TransportService transport;
+        SecurityService security;
+
+        CampusZone(){
+            library = null;
+            cafeteria = null;
+            transport = null;
+            security = null;
+
+        }
+
+        CampusZone(Library library, Cafeteria cafeteria,TransportService transport,SecurityService security)
+        {
+            this.library = library;
+            this.cafeteria = cafeteria;
+            this.transport = transport;
+            this.security = security;
+        }
+        
 }
