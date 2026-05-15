6903c2154bfdc41290e397b27767961be2d00cca
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-15 00:08:47 -0700

    to string added

--- src/com/university/Facility/Book.java
+++ src/com/university/Facility/Book.java
@@ -41,4 +41,9 @@ public class Book {
     public int getPages() {
         return pages;
     }
+
+
+    public String toString(){
+        return title + " " + " " + author + " " + " " + pages;
+    }
 }
