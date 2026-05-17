90807775a838a54b956b7f4bc1b64ecd4e28c36b
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-17 00:01:38 -0700

    admin class done

--- src/com/university/Person/Admin.java
+++ src/com/university/Person/Admin.java
@@ -1,4 +1,49 @@
 package com.university.Person;
 
-public class Admin {
+public class Admin
+{
+    private String role;
+    private String office;
+    private double salary;
+
+    Admin(String name,int age,String id,
+          String role,String office,double salary)
+    {
+        this.role = role;
+        this.office = office;
+        this.salary = salary;
+    }
+
+    public String getRole()
+    {
+        return role;
+    }
+
+    public void setRole(String role)
+    {
+        this.role = role;
+    }
+
+    public String getOffice()
+    {
+        return office;
+    }
+
+    public void setOffice(String office)
+    {
+        this.office = office;
+    }
+
+    public double getSalary()
+    {
+        return salary;
+    }
+
+    public void setSalary(double salary)
+    {
+        this.salary = salary;
+    }
+    public String toString(){
+        return role + " " + " " + office + " " + " " + salary;
+    }
 }
