ac546dea8f1f0110c80257c0efc93b8f2be365f9
MaryamCUI <maryamgulzar053cui@gmail.com>
2026-05-17 03:30:49 -0700

    dynamic scheduling done

--- src/com/university/ServiceUnit/TransportService.java
+++ src/com/university/ServiceUnit/TransportService.java
@@ -53,5 +53,20 @@ public class TransportService implements Schedulable {
         @Override
         public void generateSchedule() {
 
+            if(timing.equals("8:00 AM")) {
+
+                System.out.println("Peak hour detected...");
+                route = alternativeRoute;
+
+                System.out.println("Route updated dynamically");
+            }else{
+                route = route;
+            }
+
+            System.out.println("Transport Schedule");
+            System.out.println("Bus Number: " + busNo);
+            System.out.println("Route: " + route);
+            System.out.println("Time: " + timing);
+        }
     }
 }
